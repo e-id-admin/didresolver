62b6d47b981e078184ebc525e5d2070faff5a3b9
frithjofhoppebit <frithjof.hoppe@bit.admin.ch>
2024-07-03 16:14:24 +0200

    fix bulid script android

--- kotlin-build.sh
+++ kotlin-build.sh
@@ -1,3 +1,3 @@
 cargo build --release
+cargo ndk -o ./bindings/kotlin/jniLibs --manifest-path ./Cargo.toml -t armv7-linux-androideabi build --release
 cargo run --bin uniffi-bindgen generate --library target/release/libdidresolver.so --language kotlin --out-dir bindings/kotlin
-cp target/release/libdidresolver.so bindings/kotlin
